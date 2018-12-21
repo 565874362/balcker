a0b5064d77906bdca93c3dcd0308f5cac91a96b1
zhaodongdong <zhaodongdong@77c72aa3-25e6-483f-b639-9dae5ce950d4>
2018-12-21 05:11:22 +0000

    增加管理端

---
+++ code/Java/baihua/manage/src/main/java/com/baihua/baihuamedical/modules/sys/service/SysUserService.java
@@ -0,0 +1,56 @@
+package com.baihua.baihuamedical.modules.sys.service;
+
+import java.util.List;
+import java.util.Map;
+
+import com.baihua.baihuamedical.common.utils.PageUtils;
+import com.baihua.baihuamedical.modules.sys.entity.SysUserEntity;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+
+/**
+ * 系统用户
+ * 
+ * @author chenshun
+ * @email sunlightcs@gmail.com
+ * @date 2016年9月18日 上午9:43:39
+ */
+public interface SysUserService extends IService<SysUserEntity> {
+
+	PageUtils queryPage(Map<String, Object> params);
+
+	/**
+	 * 查询用户的所有权限
+	 * @param userId  用户ID
+	 */
+	List<String> queryAllPerms(Long userId);
+	
+	/**
+	 * 查询用户的所有菜单ID
+	 */
+	List<Long> queryAllMenuId(Long userId);
+
+	/**
+	 * 根据用户名，查询系统用户
+	 */
+	SysUserEntity queryByUserName(String username);
+
+	/**
+	 * 保存用户
+	 */
+	void dosave(SysUserEntity user);
+	
+	/**
+	 * 修改用户
+	 */
+	void update(SysUserEntity user);
+	
+
+	/**
+	 * 修改密码
+	 * @param userId       用户ID
+	 * @param password     原密码
+	 * @param newPassword  新密码
+	 */
+	boolean updatePassword(Long userId, String password, String newPassword);
+}
